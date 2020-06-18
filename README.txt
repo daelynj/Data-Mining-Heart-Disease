c9b50879340cfb8babfdb580c046ac40d338560b
daelynj <daelyndbjones@gmail.com>
2020-06-18 16:44:02 -0700

    First

---
+++ README.txt
@@ -0,0 +1,5 @@
+REQUIRED:
+
+pip install scikit-learn==0.22
+python3 -m pip install -U matplotlib
+pip install pydotplus
---
+++ decisiontrees/main.py
@@ -0,0 +1,15 @@
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.externals.six import StringIO
+from IPython.display import Image
+import matplotlib.pyplot as plt
+import pydotplus
+import pickle
+import sys
+
+
+def main():
+    print('Hello')
+
+
+if __name__ == "__main__":
+    main()
